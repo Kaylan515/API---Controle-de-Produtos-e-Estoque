6399c8e90f6633ae36b81fb57d91c293c8248226
Kaylan Teodoro <kaylanteodoro344@gmail.com>
2025-10-20 13:15:03 -0300

    Add opção de visualizar produtos ✔

--- front-end/app.py
+++ front-end/app.py
@@ -8,4 +8,14 @@ st.set_page_config(page_title="Estoque", page_icon="🏭")
 st.title("📦 Estoque de Produtos")
 
 #Menu lateral
-menu = st.sidebar.radio("Navegação", ["Catalogo", "Adicionar Produtos", "Atualizar Produtos"])
+menu = st.sidebar.radio("Navegação", ["Catalogo", "Adicionar Produto", "Atualizar Produto"])
+
+if menu == "Catalogo":
+    st.subheader("Todos os produtos disponiveis")
+    response = requests.get(f"{API_URL}/Produtos")
+    if response.status_code == 200:
+        produtos = response.json().get("produtos", [])
+        if produtos:
+            st.dataframe(produtos)
+    else:
+        st.error("Erro ao acessar a API")
